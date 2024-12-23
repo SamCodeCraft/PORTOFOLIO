52fd70088de6917cad9012e214666b0c57d74733
siyajari samuel <siyajari.samuel@student.moringaschool.com>
2024-12-23 08:55:52 +0300

    Update portofolio

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,12 +1,29 @@
+import eslint from 'eslint';
 import globals from "globals";
 import pluginJs from "@eslint/js";
 import pluginReact from "eslint-plugin-react";
+import pluginReactHooks from "eslint-plugin-react-hooks";
 
-
-/** @type {import('eslint').Linter.Config[]} */
-export default [
-  {files: ["**/*.{js,mjs,cjs,jsx}"]},
-  {languageOptions: { globals: globals.browser }},
-  pluginJs.configs.recommended,
-  pluginReact.configs.flat.recommended,
-];
+/** @type {import('eslint').Linter.Config} */
+export default {
+  overrides: [
+    {
+      files: ["**/*.{js,mjs,cjs,jsx,ts,tsx}"],
+      languageOptions: {
+        globals: globals.browser,
+      },
+      plugins: {
+        "react-hooks": pluginReactHooks,
+      },
+      rules: {
+        "no-unused-vars": "warn",
+        "react/react-in-jsx-scope": "off",
+        "react/jsx-uses-react": "off",
+        "react-hooks/rules-of-hooks": "error", // Enforces rules of hooks
+        "react-hooks/exhaustive-deps": "warn", // Warn about missing deps in useEffect
+      },
+      ...pluginJs.configs.recommended,
+      ...pluginReact.configs.recommended,
+    },
+  ],
+};
